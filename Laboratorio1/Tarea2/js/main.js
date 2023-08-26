2218732fd9c75bb7e50d03d6d536322397da2f7d
Jefersson Trejos <jeffersonbladimirlovo@gmail.com>
2023-08-25 21:35:03 -0600

    Tarea 2 Laboratorio 1

---
+++ Laboratorio1/Tarea2/js/main.js
@@ -0,0 +1,47 @@
+//Funcion para validar de los numeros
+function isValidNumber(num){
+    switch(true){
+        case isNaN(num) || num.trim() == "" || num == null:
+            return isValidNumber(prompt('El valor ingresado no es valido, debe de ser un numero:'));
+            break
+        case num <= 0:
+            return isValidNumber(prompt('El numero ingresado debe de ser mayor a 0:'));
+            break
+        default:
+            return num
+    }
+};
+
+
+//Funcion para encontrar numeros divisibles 
+function evenlyDivisible(start,end,divisor,dividers){
+    if(start <= end){
+        if(start%divisor == 0){
+            dividers.push(start);
+        }
+        start++
+        evenlyDivisible(start,end,divisor,dividers);
+    };
+    return dividers
+};
+
+
+//Parametros
+const numbers = [
+    isValidNumber(prompt('Ingrese el inicio del rango:')),
+    isValidNumber(prompt('Ingrese el final del rando:')),
+    isValidNumber(prompt('Ingrese el divisor:')) 
+];
+
+//Mostrando resultados
+const numDivisible = evenlyDivisible(numbers[0],numbers[1],numbers[2],dividers=[]);
+
+if(numDivisible.length == 0){
+    console.log('---- Arreglo vacio ----');
+}else{
+    console.log("---- Resultados ----")
+    console.log(`Inicio del rango: ${numbers[0]}`);
+    console.log(`Final del rango: ${numbers[1]}`);
+    console.log(`Divisor: ${numbers[2]}`);
+    console.log(numDivisible);
+};
